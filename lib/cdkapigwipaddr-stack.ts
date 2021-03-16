e835efd8e2a0de39876a31038dedebf4ed0c356d
suzukiken <suzukiken@gmail.com>
2021-03-16 21:46:33 +0000

    update

--- lib/cdkapigwipaddr-stack.ts
+++ lib/cdkapigwipaddr-stack.ts
@@ -1,9 +1,40 @@
 import * as cdk from '@aws-cdk/core';
+import * as apigateway from '@aws-cdk/aws-apigateway'
 
 export class CdkapigwipaddrStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
+    
+    const PREFIX_NAME = id.toLocaleLowerCase().replace('stack', '')
+    
+    const api = new apigateway.RestApi(this, 'api', { 
+      restApiName: PREFIX_NAME + '-api',
+    });
+    
+    const integration = new apigateway.MockIntegration({
+      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      requestTemplates: {
+        'application/json': JSON.stringify({
+          statusCode: 200
+        })
+      },
+      integrationResponses: [{
+        statusCode: '200',
+        responseTemplates: {
+          'application/json': JSON.stringify({
+            ip: "$context.identity.sourceIp"
+          })
+        }
+      }]
+    })
+    
+    api.root.addMethod('GET', integration, {
+      methodResponses: [{
+        statusCode: '200',
+        responseModels: {
+          'application/json': new apigateway.EmptyModel()
+        }
+      }]}
+    )
   }
 }
